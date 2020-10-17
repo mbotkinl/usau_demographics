4a8718520c11225730db38f8ef643b206e8d2185
mbotkinl <32102213+mbotkinl@users.noreply.github.com>
2020-10-17 15:31:11 -0700

    plotting improvements

--- main.py
+++ main.py
@@ -73,38 +73,52 @@ total_data_2019 = state_data_2019[MAPPING.values()].sum(axis=0)
 
 
 
+# TODO: increase text size everywhere
 
+# TODO show populations on hover?
+##### national plot
+nat_plot_data = total_data_2019.reset_index()
 
+nat_plot_data.columns = ['Race/Ethnicity', 'member_count']
+nat_plot_data['member_percent'] = nat_plot_data['member_count'] / total_data_2019.sum() * 100
+nat_plot_data = nat_plot_data.merge(census_total_data[['Race/Ethnicity', 'us_pop_percent']])
 
-
-
-
-# national plot
-plot_data = total_data_2019.reset_index()
-
-plot_data.columns = ['Race/Ethnicity', 'member_count']
-plot_data['member_percent'] = plot_data['member_count'] / total_data_2019.sum() * 100
-plot_data = plot_data.merge(census_total_data[['Race/Ethnicity', 'us_pop_percent']])
+nat_plot_data['member_percent_rounded'] = nat_plot_data['member_percent'].round(decimals=2)
+nat_plot_data['us_pop_percent_rounded'] = nat_plot_data['us_pop_percent'].round(decimals=2)
 
 
 # fig, ax = plt.subplots(figsize=(16, 20))
 # # ax1 = sns.barplot(x='Race/Ethnicity', y='member_percent', data=plot_data)
 # ax1 = sns.barplot(y='Race/Ethnicity', x='member_percent', data=plot_data)
 
-plotly_fig = go.Figure(data=[
-    go.Bar(name='USAU Member Percent', x=plot_data['Race/Ethnicity'], y=plot_data['member_percent']),
-    go.Bar(name='US Population Percent', x=plot_data['Race/Ethnicity'], y=plot_data['us_pop_percent'])
-])
-# Change the bar mode
-plotly_fig.update_layout(barmode='group')
-plot(plotly_fig)
-
-py.plot(plotly_fig, filename='national-plot', auto_open=True)
+nat_plot_data_1 = nat_plot_data.iloc[[0, 2, 5, 6]]
+nat_plot_data_2 = nat_plot_data.iloc[[1, 3, 4]]
 
 
+def nat_bar_plot(df, file_name):
+    plotly_fig = go.Figure(data=[
+        go.Bar(name='USAU Members', x=df['Race/Ethnicity'], y=df['member_percent_rounded'],
+               text=df['member_percent_rounded'], textposition='outside', hoverinfo='skip'),
+        go.Bar(name='US Census Population', x=df['Race/Ethnicity'], y=df['us_pop_percent_rounded'],
+               text=df['us_pop_percent_rounded'], textposition='outside', hoverinfo='skip')
+    ])
+    # Change the bar mode
+    plotly_fig.update_layout(barmode='group', legend=dict(orientation="h", y=1.1),
+                             xaxis_title="",
+                             yaxis_title="Percent of Group",
+                             # width=1000,
+                             # height=2000,
+                             )
+    # plot(plotly_fig)
+    py.plot(plotly_fig, filename=file_name, auto_open=True)
 
-# state plot
 
+nat_bar_plot(df=nat_plot_data_1, file_name='national-plot-1')
+nat_bar_plot(df=nat_plot_data_2, file_name='national-plot-2')
+
+
+
+### STATE DATA
 state_data_2019['state_members'] = state_data_2019[MAPPING.values()].sum(axis=1)
 melt_df = pd.melt(state_data_2019, id_vars=['State/Prov', 'state_members'], value_vars=MAPPING.values(),
                   var_name='Race/Ethnicity', value_name='member_count')
@@ -130,12 +144,15 @@ plot_df = plot_df.sort_values('state_members', ascending=False)
 
 plot_df['percent_disparity'] = plot_df['census_percent'] - plot_df['member_percent']
 
-new_max = 40
-new_min = 5
-old_max = plot_df['state_members'].max()
-old_min = plot_df['state_members'].min()
-plot_df['bubble_size'] = (((plot_df['state_members'] - old_min) * (new_max - new_min)) / (old_max - old_min)) + new_min
 
+
+
+
+
+
+
+
+##### state plot vertical
 
 MARKER_SIZE = 15
 fig = go.Figure()
@@ -146,12 +163,12 @@ fig = go.Figure()
 fig.add_trace(go.Scatter(x=plot_df['member_percent'], y=plot_df['state'],
                          mode='markers',
                          marker={'size': MARKER_SIZE},
-                         name='Member'))
+                         name='USAU Members'))
 fig.add_trace(go.Scatter(x=plot_df['census_percent'], y=plot_df['state'],
                          mode='markers',
                          marker={'size': MARKER_SIZE},
-                         name='Census'))
-fig.update_layout(width=1000, height=1500)
+                         name='US Census Population'))
+fig.update_layout(width=1000, height=1500, legend=dict(orientation="h", y=1.1), xaxis_title="Percent of Group",)
 plot(fig)
 py.plot(fig, filename='state-compare-plot', auto_open=True)
 
@@ -164,6 +181,11 @@ py.plot(fig, filename='state-compare-plot', auto_open=True)
 # plot(fig)
 
 
+
+
+
+
+#### MAP PLOT
 fig = go.Figure(data=go.Choropleth(
                 locations=plot_df['state'],  # Spatial coordinates
                 z=plot_df['percent_disparity'],  # Data to be color-coded
@@ -174,27 +196,74 @@ fig = go.Figure(data=go.Choropleth(
 
 fig.update_layout(
     title_text='2019 Racial Disparity in Ultimate',
-    geo_scope='usa', # limite map scope to USA
+    geo_scope='usa',  # limite map scope to USA
 )
 plot(fig)
 py.plot(fig, filename='state-disparity-map', auto_open=True)
 
 
+
+#### BUBBLE PLOT zoomed in
 fig = go.Figure()
 # Add traces
+fig.add_trace(go.Scatter(x=plot_df['census_percent'], y=plot_df['member_percent'],
+                         mode='markers + text',
+                         text=plot_df['state'],
+                         textposition='top center',
+                         hovertemplate=
+                         "state: %{text} <br>" +
+                         "USAU members: %{marker.size} <br>" +
+                         "census percent: %{x:.1f}%<br>" +
+                         "member percent: %{y:.1f}%<br>" +
+                         "<extra></extra>",
+                         marker=dict(
+                                size=plot_df['state_members'],
+                                sizemode='area',
+                                sizeref=2. * max(plot_df['state_members']) / (40. ** 2),
+                                sizemin=4
+                            )
+                         ))
+fig.update_xaxes(range=[-0.5, 20])
+fig.update_yaxes(range=[-0.1, 4])
+fig.update_layout(width=1000, height=1000,
+                  xaxis_title="US Census Population Percent",
+                  yaxis_title="USAU Member Percent",
+                  )
+fig.update_xaxes(zeroline=True, zerolinewidth=2, zerolinecolor='black')
+fig.update_yaxes(zeroline=True, zerolinewidth=2, zerolinecolor='black')
+plot(fig)
+py.plot(fig, filename='state-compare-xy', auto_open=True)
+
+
+
+# TODO: add color for states that are close
+#### BUBBLE PLOT proportional
+fig = go.Figure()
+# Add traces
+fig.add_trace(go.Scatter(x=[-5, 20], y=[-5, 20],  mode='lines', line=dict(color='firebrick', width=4)))
 fig.add_trace(go.Scatter(x=plot_df['census_percent'], y=plot_df['member_percent'],
                          mode='markers',
                          text=plot_df['state'],
                          hovertemplate=
                          "state: %{text} <br>" +
-                         # "state members: %{text} <br>" +
-                         "census percent: %{x:.0}%<br>" +
-                         "member percent: %{y:.0}%<br>" +
+                         "USAU members: %{marker.size} <br>" +
+                         "census percent: %{x:.1f}%<br>" +
+                         "member percent: %{y:.1f}%<br>" +
                          "<extra></extra>",
-                         marker={'size': plot_df['bubble_size']}))
-fig.update_xaxes(range=[0, 20])
-fig.update_yaxes(range=[0, 20])
-fig.update_layout(width=1000, height=1000)
-
+                         marker=dict(
+                             size=plot_df['state_members'],
+                             sizemode='area',
+                             sizeref=2. * max(plot_df['state_members']) / (40. ** 2),
+                             sizemin=4
+                         )))
+fig.update_xaxes(range=[-0.5, 20])
+fig.update_yaxes(range=[-0.1, 20])
+fig.update_layout(width=1000, height=1000,
+                  xaxis_title="US Census Population Percent",
+                  yaxis_title="USAU Member Percent",
+                  )
+fig.update_layout(showlegend=False)
+fig.update_xaxes(zeroline=True, zerolinewidth=2, zerolinecolor='black')
+fig.update_yaxes(zeroline=True, zerolinewidth=2, zerolinecolor='black')
 plot(fig)
-py.plot(fig, filename='state-compare-xy', auto_open=True)
+py.plot(fig, filename='state-compare-xy-proportional', auto_open=True)
